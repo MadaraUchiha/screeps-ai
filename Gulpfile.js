0db9a1331215bd31d16c89b15522f6c23ac5faef
Madara Uchiha <me@madara.ninja>
2016-06-21 17:03:22 +0300

    Setup linting

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,15 +3,24 @@ const webpack = require('webpack-stream');
 const gulpScreeps = require('gulp-screeps');
 const insert = require('gulp-insert');
 const credentials = require('./credentials.json');
+const eslint = require('gulp-eslint');
 
 const paths = {
     src: 'src/**/*.js'
 };
 
-gulp.task('default', () =>
+gulp.task('default', ['lint', 'commit']);
+
+gulp.task('commit', () =>
     gulp.src('src/main.js')
         .pipe(webpack({ output: { filename: "main.js" } }))
         .pipe(insert.prepend("module.exports = ")) // This line makes webpack's boilerplate be compatible with Screeps.
         .pipe(gulpScreeps(credentials)));
+
+gulp.task('lint', () =>
+    gulp.src(paths.src)
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError()));
 
 gulp.task('watch', () => gulp.watch(paths.src, ['default']));
